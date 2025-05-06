e1bd44bc7d27147b39d88e6c26ca23beed707ca4
Jhoan <nic3n1217@gmail.com>
2025-05-06 09:51:29 -0500

    lista, tuplas, diccionarios, sets, funciones

--- python/main.py
+++ python/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+#listas
+frutas = ["manzana","pera","uva","coco"]
+matriz = [["nicolas","ruben","stefanny"],[16,16,16],["Hombre","Hombre","Mujer"]]
+#diccionarios
+colores = {"Red":(211, 43, 43),"Blue":(223, 120, 51),"Green":(125, 218, 88)}
+
+#tuplas
+RED = (211, 43, 43)
+cordenadas= (4,8,4)
+
+#set
+
+#funciones
